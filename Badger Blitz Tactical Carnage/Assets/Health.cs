262eb475b168157b1320b12911a858e0836e75df
KungeniDjunglen <thhe05001@utb.vaxjo.se>
2023-12-19 21:02:18 +0100

    lite

--- Badger Blitz Tactical Carnage/Assets/Health.cs
+++ Badger Blitz Tactical Carnage/Assets/Health.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     public float health;
+    public bool IsBoss = false;
+    private PlayerMovement playerMovement;
+    public TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void TakeDamage(float damage){
         health = health - damage;
         if (health <= 0){
+            if(IsBoss){
+                playerMovement.enabled = !playerMovement.enabled;
+                text.text = "Du vann";
+                Debug.Log("Du vann");
+            }
             Destroy(gameObject);
+
         }
     }
 }
